8b46a9a8263ef0eab24118fd3342f0e3a2fc609a
julfinch <julfinch@gmail.com>
2023-02-09 01:10:30 +0800

    added sync key

--- admin-dashboard-syncfusion/src/index.jsx
+++ admin-dashboard-syncfusion/src/index.jsx
@@ -6,9 +6,11 @@ import "./index.css";
 import App from "./App";
 import { ContextProvider } from './contexts/ContextProvider';
 import { UserProvider } from './contexts/UserContext';
-
+import { registerLicense } from '@syncfusion/ej2-base';
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+// Registering Syncfusion license key
+registerLicense('@32302e342e30KS7S2eMQ3+qakLZr2uBDJ+cDSYTkaUFD9ecO9meSyfA=');
 
 root.render(
   <StrictMode>
